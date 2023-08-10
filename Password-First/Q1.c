bb03fc5f8aec6870d9c2e158b4860d6e8c0c1171
v1n0th-H4ck3r <vinoth.dayalanofficial17@gmail.com>
2023-08-10 15:12:10 +0530

    Working one for Login

--- Password-First/Q1.c
+++ Password-First/Q1.c
@@ -1,50 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(){
-    char username[40];
-    char Password[40];
-    char AuthUser[3][50] = {"Geek", "Geeks", "Geekfor"};
-    char AuthPass[3][50] = {"Geek", "Geeks", "Geekfor"};
-    int i=0,key,j=0;
-    while(i!=3){
-        i=i+1;
-        printf("Enter the username: ");
-        scanf("%s",&username);
-        for(j=0;j<3;j++){
-            printf("%d\n",j);
-            if(username==AuthUser[j]){
-                key = j;
-                printf("%d",key);
+    char *UserName[] = {"Test","test","TeSt","tEsT","TEST"};
+    char *UserPass[] = {"Test","test","TeSt","tEsT","TEST"};
+    char InputUser[10],InputPass[10];
+    printf("Enter username: ");
+    scanf("%s",&InputUser);
+    printf("Enter password: ");
+    scanf("%s",&InputPass);
+    for(int i=0;i<2;i++){
+        if(strcmp(InputUser, UserName[i]) == 0){
+            if(strcmp(InputPass, UserPass[i]) == 0){
+                printf("Login Sucess\n");
                 break;
+            }else{
+             printf("Username or password is wrong\n");
+             break;
             }
-        }
-        
-        if(key){
-            printf("Enter the password: ");
-            scanf("%s",&Password);
-            if(Password==AuthPass[key]){
-                    printf("Authentication Successful \n");
-                    break;
-                }else{
-                    printf("Authentication failed, try again \n");
-                }
         }else{
-                printf("Try Again, Username miss match \n");
+             printf("Username or password is wrong\n");
+             break;
         }
     }
-    }
-    // for(int i=0;i<3;i++){
-    //     printf("Enter the username: ");
-    //     scanf("%s",&username);
-    //     if(username==AuthUser[i]){
-    //         printf("Enter the password: ");
-    //         scanf("%s",&Password);
-    //         if(Password==AuthPass[i]){
-    //             printf("Authentication Successful");
-    //             break;
-    //         }else{
-    //             printf("Authentication failed, try again");
-    //         }
-    //     }else{
-    //         printf("Try Again, Username miss match");
-    //     }
-    // }
+}
